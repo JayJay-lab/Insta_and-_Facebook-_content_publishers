c229fde7f62fba234cc103442b82d1d00f7e05fe
JayJay-lab <145923076+JayJay-lab@users.noreply.github.com>
2025-07-19 21:43:12 +0200

    This is the main function

---
+++ main.py
@@ -0,0 +1,26 @@
+import Instagramgittest
+
+
+def main():
+
+
+
+
+     
+   
+
+    # Login to Instagram and perform media uploads
+
+    Instagramgittest.login_info()
+    Instagramgittest.action_selection()
+
+
+
+
+    
+    
+if __name__ == "__main__":
+    main()
+
+
+    
